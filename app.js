524e0eb807366154c35e3923f5a13d3ce88dbd9f
Genevieve <vievenavales29@gmail.com>
2021-09-14 19:35:54 +0800

    background color changing

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 "use strict";
+
+const sections = document.querySelectorAll(".section__issue");
+const body = document.querySelector("body");
+
+let currentSection,
+  pastSection = "section-1";
+
+document.addEventListener("scroll", () => {
+  sections.forEach((s) => {
+    const section = s.getBoundingClientRect();
+    if (section.bottom > 0 && section.bottom < window.innerHeight * 1.2) {
+      currentSection = s.id;
+    }
+  });
+
+  const currentColor = document.getElementById(`${currentSection}`).dataset
+    .color;
+  if (currentSection !== pastSection) changeColor(currentColor);
+});
+
+const changeColor = (color) => {
+  pastSection = currentSection;
+  $("body").css("background-color", `#${color}`);
+};
