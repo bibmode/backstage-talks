5fe0026c6c410a193c6777cfc1e618d92d7fe048
Genevieve <vievenavales29@gmail.com>
2021-09-16 01:36:41 +0800

    added fullpage library scroll

--- app.js
+++ app.js
@@ -1,5 +1,61 @@
 "use strict";
 
+const links = document.querySelectorAll(".issue-link");
+const container = document.querySelector(".container");
+
+let position = $(window).scrollTop();
+
+$(window).scroll(function () {
+  let scroll = $(window).scrollTop();
+  if (scroll > position) {
+    console.log("scrollDown");
+  } else {
+    console.log("scrollUp");
+  }
+  position = scroll;
+  console.log(position);
+});
+
+new fullpage("#fullpage", {
+  //options here
+  //autoScrolling: true,
+  scrollHorizontally: true,
+});
+
+/////////////////////////////////////////////
+// scroll snap
+
+// getHeightOfSections();
+// let initialScroll = 0,
+//   newScroll;
+
+// document.addEventListener("scroll", function (e) {
+//   const nextScroll = document.documentElement.scrollTop;
+//   console.log(nextScroll, initialScroll);
+
+//   if (nextScroll > initialScroll) {
+//     container.style.transform = `translate3d(0, -100vh ,0)`;
+//   }
+
+//   if (nextScroll < initialScroll) {
+//     container.style.transform = `translate3d(0, 100vh ,0)`;
+//   }
+
+//   initialScroll = nextScroll;
+// });
+// let sectionNow = 0;
+// window.onscroll = function (e) {
+//   const scrollDirection = this.oldScroll > this.scrollY;
+//   console.log(scrollDirection);
+//   this.oldScroll = this.scrollY;
+//   // if(scrollDirection) s
+// };
+
+// document.addEventListener("scroll", function () {
+//   console.log("hello");
+//   scrollDirection = false;
+// });
+
 /////////////////////////////////////////////
 //color changing in the background on scroll
 
@@ -20,7 +76,10 @@ document.addEventListener("scroll", () => {
   const currentColor = document.getElementById(`${currentSection}`).dataset
     .color;
 
-  if (currentSection !== pastSection) changeColor(currentColor);
+  if (currentSection !== pastSection) {
+    changeColor(currentColor);
+    snapScroll(currentSection);
+  }
 });
 
 const changeColor = (color) => {
@@ -29,52 +88,51 @@ const changeColor = (color) => {
 };
 
 /////////////////////////////////////////////
+// snap scrolling
+
+const snapScroll = (currentDiv) => {
+  const sectionNow = document.getElementById(`${currentDiv}`);
+  //console.log(sectionNow.parentElement);
+};
+
+/////////////////////////////////////////////
 // scroll on click link
 
-const links = document.querySelectorAll(".issue-link");
-const container = document.querySelector(".container");
-
-let sectionsArr = [];
+let currentBook = 1;
 
 links.forEach((link) => {
   link.addEventListener("click", (e) => {
     e.preventDefault();
     scrollOnLink(link.dataset.num);
-    console.log(link.dataset.num);
+    //console.log(link.dataset.num);
   });
 });
 
-// const getHeightOfSections = function () {
-//   links.forEach((link) => {
-//     //console.log(link);
+const getHeightOfSections = function () {
+  let sectionsArr = [];
+  links.forEach((link) => {
+    //console.log(link);
 
-//     const targetSection = document.querySelector(`#book-${link.dataset.num}`);
-//     console.log(targetSection);
+    const targetSection = document.querySelector(`#book-${link.dataset.num}`);
+    //console.log(targetSection);
 
-//     const sectionPosition = targetSection.getBoundingClientRect();
-//     console.log(sectionPosition);
+    const sectionPosition = targetSection.getBoundingClientRect();
+    //console.log(sectionPosition);
 
-//     sectionsArr.push(sectionPosition.top);
-//   });
-//   console.log(sectionsArr);
-// };
-
-// getHeightOfSections();
-
-const scrollOnLink = (targetIndex) => {
-  container.style.transform = `translate3D(0 , calc(100vh * -${
-    targetIndex - 1
-  }), 0)`;
+    sectionsArr.push(sectionPosition.top);
+  });
+  return sectionsArr;
 };
 
-// const scrollOnLink = (choice) => {
-//   const num = choice.dataset.num;
-//   console.log(num);
-
-//   const targetSection = document.querySelector(`#book-${num}`);
-//   console.log(targetSection);
-//   const bottom = targetSection.getBoundingClientRect().bottom;
-//   console.log(bottom);
-
-//   container.style.transform = `translateY(100vh)`;
+// let direction;
+// window.onscroll = function (e) {
+//   const scrollDirection = this.oldScroll > this.scrollY;
+//   console.log(scrollDirection);
+//   this.oldScroll = this.scrollY;
 // };
+
+const scrollOnLink = (targetIndex) => {
+  container.style.transform = `translateY(calc(100vh * -${
+    Number(targetIndex) - 1
+  }))`;
+};
