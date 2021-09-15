5a48609ae5e8d8d6adecbe4a03753c1621442001
Genevieve <vievenavales29@gmail.com>
2021-09-16 02:48:04 +0800

    changing background colors

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const links = document.querySelectorAll(".issue-link");
 const container = document.querySelector(".container");
 
+const colors = ["#00c1b5", "#ff651a", "#ffbe00", "#1d3fbb", "#e30512"];
+
 new fullpage("#fullpage", {
   //options here
   anchors: ["section1", "section2", "section3", "section4", "section5"],
@@ -11,18 +13,15 @@ new fullpage("#fullpage", {
   scrollHorizontally: true,
   dropEffect: true,
 
-  afterSlideLoad: function (section, origin, destination, direction) {
-    var loadedSlide = this;
+  onLeave: function (origin, destination, direction) {
+    let leavingSection = this;
 
-    //first slide of the second section
-    if (section.anchor == "section2" && destination.index == 1) {
-      console.log("First slide loaded");
-    }
+    $("body").css("background-color", colors[destination.index]);
 
-    //second slide of the second section (supposing #secondSlide is the
-    // //anchor for the second slide)
-    // if (section.index == 1 && destination.anchor == "secondSlide") {
-    //   console.log("Second slide loaded");
+    // if (origin.index == 1 && direction == "down") {
+    //   alert("Going to section 3!");
+    // } else if (origin.index == 1 && direction == "up") {
+    //   alert("Going to section 1!");
     // }
   },
 });
@@ -30,34 +29,3 @@ new fullpage("#fullpage", {
 document.addEventListener("change", function () {
   console.log("change");
 });
-
-/////////////////////////////////////////////
-//color changing in the background on scroll
-
-const sections = document.querySelectorAll(".section__issue");
-const body = document.querySelector("body");
-
-let currentSection,
-  pastSection = "section-1";
-
-document.addEventListener("scroll", () => {
-  sections.forEach((s) => {
-    const section = s.getBoundingClientRect();
-    if (section.bottom > 0 && section.bottom < window.innerHeight * 1.2) {
-      currentSection = s.id;
-    }
-  });
-
-  const currentColor = document.getElementById(`${currentSection}`).dataset
-    .color;
-
-  if (currentSection !== pastSection) {
-    changeColor(currentColor);
-    snapScroll(currentSection);
-  }
-});
-
-const changeColor = (color) => {
-  pastSection = currentSection;
-  $("body").css("background-color", `#${color}`);
-};
